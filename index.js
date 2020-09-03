cf5f28cf5e06e11b65dcd77a8aeb67a9f7161145
Zac Zacal <Jizacal@outlook.com>
2020-09-02 19:10:21 -0700

    Added more patterns

--- index.js
+++ index.js
@@ -42,10 +42,11 @@ let handleMessage = (message) => {
         }
     });
     return matched;
-}
+};
 
 const patterns = [
     [new RegExp(/natalie/i), "imbruglia"],
     [new RegExp(/rickroll/i), "rickroll"],
-    [new RegExp(/give you up/i), "rickroll"]
-]
+    [new RegExp(/give you up/i), "rickroll"],
+    [new RegExp(/never gonna/i), "rickroll"]
+];
