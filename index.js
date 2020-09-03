9d9253ce13f69fba4fa160c676836f13845e7e60
Zac Zacal <Jizacal@outlook.com>
2020-09-02 17:01:58 -0700

    Reply to torn

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
 const client = new Discord.Client();
+const pattern = new RegExp(/torn/i);
+const tornLimit = 5000 // 5 second limit
+var lastTornt = null;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.login(config.token);
+
+client.on('message', message => {
+    if(message.author.id != client.user.id){
+        if(!lastTornt || lastTornt < (new Date() - tornLimit) && pattern.exec(message)) {
+            message.channel.send(`Last torn: ${lastTornt || 'never'}`);
+            message.channel.send('https://giphy.com/gifs/quQ5pJEjeZwTm');
+            lastTornt = new Date();
+        }
+    }
+});
