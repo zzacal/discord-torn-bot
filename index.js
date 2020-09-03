785587d22255fb6b47f5d92160b7b761a7be2f65
Zac Zacal <Jizacal@outlook.com>
2020-09-02 18:24:12 -0700

    Configable all the things

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+//configuration
+const tornLimit = config.limit; // 60 second limit
+const discord_token = config.token;
+const giphy_key = config.giphy_key;
+
 const Discord = require('discord.js');
 const config = require('./config.json');
-var giphy = require('giphy')('dc6zaTOxFJmzC');
 const client = new Discord.Client();
+var giphy = require('giphy')(giphy_key);
 const pattern = new RegExp(/torn/i);
-const tornLimit = 5000 // 5 second limit
 var lastTornt = null;
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(discord_token);
 
 client.on('message', message => {
     if(message.author.id != client.user.id){
         if(!lastTornt || lastTornt < (new Date() - tornLimit)) {
             if (pattern.exec(message)){
-                message.channel.send(`Last torn: ${lastTornt || 'never'}`);
                 let any_six = Math.floor(Math.random() * 5);
-                giphy.search({q:"natalie imbruglia", api_key:"dc6zaTOxFJmzC"}, (_,results)=> {
+                giphy.search({q:"imbruglia"}, (_,results)=> {
                     message.channel.send(results.data[any_six].embed_url)
                 });
                 lastTornt = new Date();
