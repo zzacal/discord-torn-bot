32d54bde543b9313dc7a19b497b0f01e19978bc3
Zac Zacal <jizacal@outlook.com>
2020-09-03 02:54:41 -0700

    Added torn

--- patterns.js
+++ patterns.js
@@ -1,5 +1,6 @@
 const patterns = [
     [new RegExp(/natalie/i), "imbruglia"],
+    [new RegExp(/torn/i), "imbruglia"],
     [new RegExp(/rickroll/i), "rickroll"],
     [new RegExp(/give you up/i), "rickroll"],
     [new RegExp(/never gonna/i), "rickroll"]
