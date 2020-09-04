9d314c48f751946f1fadf4d0cb67dc2352911f77
Zac Zacal <Jizacal@outlook.com>
2020-09-03 23:25:39 -0700

    Added haddaway

--- patterns.js
+++ patterns.js
@@ -3,7 +3,8 @@ const patterns = [
     [new RegExp(/torn/i), "imbruglia"],
     [new RegExp(/rickroll/i), "rickroll"],
     [new RegExp(/give you up/i), "rickroll"],
-    [new RegExp(/never gonna/i), "rickroll"]
+    [new RegExp(/never gonna/i), "rickroll"],
+    [new RegExp(/love/i), "haddaway"]
 ];
 
 exports.patterns = patterns;
