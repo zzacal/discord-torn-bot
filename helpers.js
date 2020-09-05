26997d7cd8044cbc509454ac3d9eb8c8acdbd515
Zac Zacal <Jizacal@outlook.com>
2020-09-04 19:25:11 -0700

    Syntax fixes

--- helpers.js
+++ helpers.js
@@ -1,19 +1,19 @@
 let idif = (message, condition) => {
     if (condition) {
-        return message;
+        return true;
     }
     return false;
 }
 
-let handleCommand = (message, commands) => {
-    return message;
+let handleCommand = (message, commands = []) => {
+    return true;
 }
 
 let handleMessage = (message, patterns, getGiph) => {
     let matched = false;
     patterns.forEach((pattern) => {
         if (pattern[0].exec(message.content)) {
-            console.log(`${new Date()} Matched ${pattern[0]} | ${message.author.username}: ${message.content}`)
+            console.log(`${new Date()} Matched ${pattern[0]} | ${message.author.username}: ${message.content}`);
             matched = true;
             getGiph(pattern[1], (giph) => message.channel.send(giph));
         }
@@ -26,7 +26,7 @@ let getGiph = (giphy) => {
         giphy.search({q:query, limit: 20}, (err,results) => {
             err && console.log(`${new Date()}: giphy error: ${err}`);
             const maxi = results.data.length - 1;
-            let rndi = Math.floor(Math.random() * maxi)
+            let rndi = Math.floor(Math.random() * maxi);
             handler(results.data[rndi].embed_url);
         });
     }
