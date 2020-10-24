2d7968c22a485bf8d122b08c8e2b9a4adf37f7c9
zzacal <zac.zacal@alaskaair.com>
2020-10-24 02:06:56 -0700

    Use envs for token and giphy key

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker stop discord-torn-bot &&
-docker container rm discord-torn-bot &&
+docker stop discord-torn-bot || true &&
+docker container rm discord-torn-bot || true &&
 docker build -t discord-torn-bot:1 . &&
-docker run -it --name=discord-torn-bot  discord-torn-bot:1
+docker run -d --name=discord-torn-bot \
+    -e DISCORD_TOKEN=$DISCORD_TOKEN \
+    -e GIPHY_KEY=$GIPHY_KEY \
+    discord-torn-bot:1
